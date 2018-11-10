7a156280854485ce7d8d06a17cf193cc41854d59
Mathias Arredondo <matou@MacBook-Pro-de-Mathias.local>
2018-11-10 18:37:16 +0100

    Contactez nous + merge Mattou modif

--- src/pages/contacteznous/contacteznous.ts
+++ src/pages/contacteznous/contacteznous.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { PlatPage } from '../plat/plat';
 import { GateauPage } from '../gateau/gateau';
 import { ComptePage } from '../compte/compte';
@@ -19,14 +19,89 @@ import { PanierPage } from '../panier/panier';
 })
 export class ContacteznousPage {
 
+  //Variable to set the choice of the user (Livraison / A emporter / Sur place)
   choix: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  //Variables to contact :
+  lastName: string = "";
+  firstName: string = "";
+  email: string = "";
+  phone: string = "";
+  select: string = "";
+  message: string = "";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toastCtrl: ToastController) {
     this.choix = this.navParams.get('choix') ? this.navParams.get('choix') : "livraison";
   }
 
-  ionViewDidLoad() {
+  //Check if the user entered a good email
+  isEmailValid(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
   }
+
+  //Check if the string contains number :
+  hasNumber(myString) {
+    return /\d/.test(myString);
+  }
+
+  //Display a popup when there is a problem :
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+    });
+
+    toast.present();
+  }
+
+  //Function to send a mail : 
+  sendMessage() {
+    if (this.lastName.length < 2) {
+      this.presentToast("Il faut au minimum 2 caractères dans le nom");
+      return;
+    }
+    if (this.hasNumber(this.lastName) == true) {
+      this.presentToast("Votre nom ne peut pas contenir de chiffre");
+      return;
+    }
+    if (this.firstName.length < 2) {
+      this.presentToast("Il faut au minimum 2 caractères dans le prénom");
+      return;
+    }
+    if (this.hasNumber(this.firstName) == true) {
+      this.presentToast("Votre prénom ne peut pas contenir de chiffre");
+      return;
+    }
+    if (this.email.length < 6) {
+      this.presentToast("Il faut au minimum 6 caractères dans l'adresse mail");
+      return;
+    }
+    if (this.isEmailValid(this.email) == false) {
+      this.presentToast("Adresse email invalide");
+      return;
+    }
+    if (this.phone.length != 10) {
+      this.presentToast("Il faut 10 chiffres pour le numéro de téléphone");
+      return;
+    }
+    if (isNaN(parseInt(this.phone))) {
+      this.presentToast("Le téléphone ne doit contenir que des chiffres");
+      return;
+    }
+    if (this.select.length < 2) {
+      this.presentToast("Veuillez sélectionner un motif pour votre message");
+      return;
+    }
+    if (this.message.length < 5) {
+      this.presentToast("Veuillez saisir un message");
+      return;
+    }
+
+
+  };
+
+  //Function to redirect the user to other pages :
   redirectionMonCompte() {
     this.navCtrl.setRoot(ComptePage, {choix: this.choix});
   }
